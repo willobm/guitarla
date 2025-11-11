6d91f48c9abf6d93941fa9ff7aab542e110a3937
Wilson Flores <wilson.flores@centrohub.co>
2025-11-10 22:47:05 -0500

    fix: add name company

--- src/App.jsx
+++ src/App.jsx
@@ -149,7 +149,7 @@ function App() {
 
     <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
-            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados-Syncwares</p>
         </div>
     </footer>
 
